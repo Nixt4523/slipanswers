3e813e8f3721e9ef0b530d5bd7a9f12a490e400c
Nikhil <nikhilthoratofficial@gmail.com>
2024-04-14 22:38:08 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const answerContainer = document.getElementById("answerContainer");
+
+for (let i = 1; i <= 30; i++) {
+	const link = document.createElement("a");
+	link.classList.add("btn");
+	link.innerText = `Slip ${i}`;
+	link.href = `./public/Slip${i}.txt`;
+	link.download = `${i}.txt`;
+
+	answerContainer.append(link);
+}
