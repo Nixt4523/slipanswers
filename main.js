bec373afb62d0fc1ee9087a5fa03eab557e6ab73
Nikhil <94195544+Nixt4523@users.noreply.github.com>
2024-04-15 09:46:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const answerContainer = document.getElementById("answerContainer");
 
-for (let i = 1; i <= 30; i++) {
+for (let i = 1; i <= 31; i++) {
 	const link = document.createElement("a");
 	link.classList.add("btn");
 	link.innerText = `Slip ${i}`;
